2c432469b57abbcd9337cc3fa50fb80dead40a53
Charles Alexander <starwarsrunner15@gmail.com>
2021-04-29 21:39:23 -0500

    Add files via upload

---
+++ Program4.cs
@@ -0,0 +1,21 @@
+﻿//Charles Alexander
+using System;
+
+namespace CharlesAlexander4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Count Down!!!");
+            for (int a = 19; a > 0; a -= 2)
+            {
+                Console.WriteLine("{0}", a);
+
+               
+            }
+            Console.WriteLine("Press any number");
+
+        }
+    }
+}
