5ec6cee255636462272bdb82b7b9e6a5702f7e78
Charles Alexander <starwarsrunner15@gmail.com>
2021-04-29 21:39:06 -0500

    Add files via upload

---
+++ Program3.cs
@@ -0,0 +1,51 @@
+﻿//Charles Alexnader
+using System;
+
+namespace CharlesAlexander3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            int y;
+
+            Console.WriteLine("Put in two numbers..");
+            String x = Console.ReadLine();
+            Console.WriteLine("enter the first number ");
+            String y = Console.ReadLine();
+
+            int num;
+            num = Convert.ToInt32(Console.ReadLine());
+
+            String message = "The numbers are now";
+            Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
