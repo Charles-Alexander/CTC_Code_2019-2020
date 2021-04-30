cbb14c8b9a1d2657be8a0a780f43d15a02d2f141
Charles Alexander <starwarsrunner15@gmail.com>
2021-04-29 21:39:56 -0500

    Add files via upload

---
+++ Program6.cs
@@ -0,0 +1,46 @@
+﻿//Charles Alexander
+//Project #7
+
+using System;
+
+namespace CharlesAlexander6_MultiDimenArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int[,] grades1 = { { 0, 0 }, { 1, 2 }, { 2, 4 }, { 3, 6 }, { 4, 8 } };
+
+            int i;
+            int j;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
