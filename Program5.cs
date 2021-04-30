85061fb4360806cf7e9358f38425e258dede417a
Charles Alexander <starwarsrunner15@gmail.com>
2021-04-29 21:39:41 -0500

    Add files via upload

---
+++ Program5.cs
@@ -0,0 +1,27 @@
+﻿//Charles Alexander
+using System;
+
+namespace CharlesAlexander5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array1 = { 1, 2, 3 };
+            int[] array2 = new int[] { 1, 2, 3 };
+
+            string[] array3 = { "dot", "net", "perals" };
+            string[] array4 = new string[] { "DOT", "NET", "PERALS", null };
+
+
+            Console.WriteLine("array1: {0} length", array1.Length);
+            Console.WriteLine("array2: {0} length", array2.Length);
+            Console.WriteLine("array3: {0} length", array3.Length);
+            Console.WriteLine("array4: {0} length", array4.Length);
+
+
+
+
+        }
+    }
+}
