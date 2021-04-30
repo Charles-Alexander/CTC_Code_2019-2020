cd05a0d22457a520f112ca5397ffdfcfcdb5aa12
Charles Alexander <starwarsrunner15@gmail.com>
2021-04-29 19:22:12 -0500

    Add files via upload

---
+++ CharlesAlexander_17_Python_Project_4.py
@@ -0,0 +1,9 @@
+#Charles Alexander Project #4
+
+letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y","Z"]
+
+print ("Hello. Welcome to The Caesar Cipher.")
+def main():
+	mainmenu = input("What do you want to do?\n[1] Encode a String\n[2] Decode a String\n[3] Crack an encrypted String\n[4]Ouit ")
+
+
