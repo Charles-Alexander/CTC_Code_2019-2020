142bd2bd57b951018910f2503eebb4661eff50dd
Charles Alexander <starwarsrunner15@gmail.com>
2021-04-29 21:38:26 -0500

    Add files via upload

---
+++ Program2.cs
@@ -0,0 +1,24 @@
+﻿// Charles Alexander
+// Project #3 V2
+
+using System;
+
+namespace CharlesAlexander2_V2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            Console.WriteLine("While loop Example");
+            while (a < 11)
+            {
+
+
+                Console.WriteLine($"my count is {a} ");
+                a++;
+            }
+            Console.ReadLine();
+        }
+    }
+}
